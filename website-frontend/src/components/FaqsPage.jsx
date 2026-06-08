22f04e5567ca85c1c63e21f7ee43ec01c6ce97ff
Amit Yadav <YOUR_GITHUB_EMAIL>
2026-06-08 20:18:35 +0530

    Allow full FAQ card copy text

--- website-frontend/src/components/FaqsPage.jsx
+++ website-frontend/src/components/FaqsPage.jsx
@@ -270,7 +270,6 @@ export default function FaqsPage({ data = {} }) {
           color: #111111;
           margin: 0;
           display: -webkit-box;
-          -webkit-line-clamp: 2;
           -webkit-box-orient: vertical;
           overflow: hidden;
         }
